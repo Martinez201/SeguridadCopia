e31c65ea0a56470925251c349eb308f853ae1d90
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2020-03-29 22:13:32 +0200

    Generada Entidad Parte

---
+++ src/AppBundle/Entity/Parte.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="parte")
+ */
+class Parte
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="date")
+     * @var \DateTime
+     */
+    private $fecha;
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $observaciones;
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    private $estado;
+
+    /**
+     * @return \DateTime
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * @param \DateTime $fecha
+     * @return Parte
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getObservaciones()
+    {
+        return $this->observaciones;
+    }
+
+    /**
+     * @param string $observaciones
+     * @return Parte
+     */
+    public function setObservaciones($observaciones)
+    {
+        $this->observaciones = $observaciones;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * @param bool $estado
+     * @return Parte
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+
+}
