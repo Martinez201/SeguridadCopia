7e106d826ac8c993de48e2caa5b765b7e0bf23a7
Martinez201 <jesus.martinez.gonzalez1993@gmail.com>
2020-04-16 12:53:24 +0200

    Mantenimiento completo de la entidad Delegación

--- src/AppBundle/Controller/DelegacionController.php
+++ src/AppBundle/Controller/DelegacionController.php
@@ -4,8 +4,11 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Delegacion;
+use AppBundle\Form\Type\DelegacionType;
 use AppBundle\Repository\DelegacionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DelegacionController extends Controller
@@ -15,13 +18,96 @@ class DelegacionController extends Controller
      * @Route("/delegaciones", name="delegaciones_listar")
      */
 
-    public function delegacionesAction(DelegacionRepository $delegacionRepository){
+    public function delegacionesAction(DelegacionRepository $delegacionRepository)
+    {
 
         $delegaciones = $delegacionRepository->obtenerDelegaciones();
 
-        return $this->render('delegaciones/listar.html.twig',[
+        return $this->render('delegaciones/listar.html.twig', [
 
             'delegaciones' => $delegaciones
         ]);
     }
+
+    /**
+     * @Route("/delegacion/alta", name="altas_delegaciones", methods={"GET","POST"})
+     */
+
+    public function nuevaAction(Request $request)
+    {
+
+        $delegacion = new Delegacion();
+        $this->getDoctrine()->getManager()->persist($delegacion);
+        return $this->formAction($request, $delegacion);
+    }
+
+
+    /**
+     * @Route("/delegacion/{id}", name="delegaciones_form", requirements={"id" = "\d+"}, methods={"GET","POST"})
+     */
+
+    public function formAction(Request $request, Delegacion $delegacion)
+    {
+
+        $form = $this->createForm(DelegacionType::class, $delegacion);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            try {
+
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
+                $this->addFlash('success', 'Se han guardado los datos con éxito');
+                return $this->redirectToRoute('delegaciones_listar');
+
+            } catch (\Exception $ex) {
+
+                $this->addFlash('error', 'Error: no se ha podido guardar los cambios');
+            }
+
+        }
+
+        return $this->render('delegaciones/from.html.twig', [
+
+            'form' => $form->createView(),
+            'delegacion' => $delegacion
+
+        ]);
+
+    }
+
+    /**
+     * @Route("/delegacion/eliminar/{id}", name="delegacion_eliminar", requirements={"id" = "\d+"}, methods={"GET","POST"})
+     */
+
+    public function eliminarAction(Request $request, Delegacion $delegacion)
+    {
+
+        if ($request->getMethod() == 'POST') {
+
+
+            try {
+
+                $cli = $this->getDoctrine()->getManager();
+                $cli->remove($delegacion);
+                $cli->flush();
+                $this->addFlash('success', 'Delegación dada de baja con éxito');
+                return $this->redirectToRoute('delegaciones_listar');
+
+            } catch (\Exception $ex) {
+
+                $this->addFlash('error', 'Error: no se ha podido dar de baja a la Delegación');
+            }
+
+        }
+
+        return $this->render('delegaciones/eliminar.html.twig', [
+
+            'delegacion' => $delegacion
+
+        ]);
+
+    }
+
 }
---
+++ src/AppBundle/Form/Type/DelegacionType.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace AppBundle\Form\Type;
+
+
+use AppBundle\Entity\Delegacion;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DelegacionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre', TextType::class,[
+
+                'label'=>'Nombre de la delegación:'
+            ])
+            ->add('direccion', TextType::class,[
+
+                'label'=>'Dirección:'
+            ])
+            ->add('ciudad', TextType::class,[
+
+                'label'=> 'Ciudad:'
+
+            ])
+            ->add('provincia', TextType::class,[
+
+                'label'=> 'Provincia:'
+            ])
+            ->add('cPostal', TextType::class, [
+
+                'label'=> 'Código Postal:'
+
+            ])
+            ->add('telefono', TextType::class,[
+
+                'label'=> 'Teléfono:'
+
+            ])
+            ->add('email', TextType::class,[
+
+                'label'=>'Email:'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+        $resolver->setDefaults([
+
+            'data_class'=> Delegacion::class
+
+        ]);
+
+    }
+
+}
